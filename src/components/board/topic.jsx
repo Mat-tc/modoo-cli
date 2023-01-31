158c461760e6196cfd05bf9ed26c2439a029f4a6
gajeong <rlarkwjd127@gmail.com>
2023-01-31 19:19:52 +0900

    grid 적용해야함

--- src/components/board/topic.jsx
+++ src/components/board/topic.jsx
@@ -1,17 +1,22 @@
 import React, { useContext } from 'react'
 import classNames from 'classnames'
 import { ThemeContext } from '@/App'
-//import styles from './topic.module.css'
+import styles from './topic.module.css'
+import button from '@css/button.module.css'
 const Topic = (props) => {
   const { darkMode } = useContext(ThemeContext)
   return (
     <div className={classNames('clay', 'card', darkMode ? 'dark' : 'light')}>
-      <dd>💬 {props.title}</dd>
-      <br />
-      <li>Palestinian Territories</li>
-      <li>Madagascar</li>
-      <li>Congo - Brazzaville</li>
-      <li>Poland</li>
+      <div className={styles.titleContainer}>
+        <dd>💬 {props.title}</dd>
+        <dd className={classNames(styles.gotoLink, button.btn)}>바로가기</dd>
+      </div>
+      <div className={styles.ranks}>
+        <li>Palestinian Territories</li>
+        <li>Madagascar</li>
+        <li>Congo - Brazzaville</li>
+        <li>Poland</li>
+      </div>
     </div>
   )
 }
