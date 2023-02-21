2588225c0e36c29a956710b54caa7546fc0c18f4
gajeong <rlarkwjd127@gmail.com>
2023-02-21 23:26:54 +0900

    install tinymce library

--- src/components/write-board/editor.jsx
+++ src/components/write-board/editor.jsx
@@ -1,5 +1,5 @@
-import React, { useRef } from 'react'
-import { Editor } from '@tinymce/tinymce-react'
+import React, { useRef } from "react"
+import { Editor } from "@tinymce/tinymce-react"
 
 export default function EditorBoard() {
   const editorRef = useRef(null)
@@ -17,15 +17,15 @@ export default function EditorBoard() {
           height: 500,
           menubar: false,
           plugins: [
-            'advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount',
+            "advlist autolink lists link image charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table paste code help wordcount",
           ],
           toolbar:
-            'undo redo | formatselect | ' +
-            'bold italic backcolor | alignleft aligncenter ' +
-            'alignright alignjustify | bullist numlist outdent indent | ' +
-            'removeformat | help',
+            "undo redo | formatselect | " +
+            "bold italic backcolor | alignleft aligncenter " +
+            "alignright alignjustify | bullist numlist outdent indent | " +
+            "removeformat | help",
         }}
       />
     </>
