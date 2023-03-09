2952a45c6d334d09806cbef1de4c3a29fba4ebeb
gajeong <rlarkwjd127@gmail.com>
2023-03-09 17:30:06 +0900

    err 해결

--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -1,38 +1,38 @@
-import React from "react";
-import { Introduce } from "./Introduce";
-import "./mypage.css";
-import { UserInfo } from "./UserInfo";
-import { UsingPlace } from "./UserPlace";
+import React from 'react'
+import { Introduce } from './Introduce'
+import './mypage.css'
+import { UserInfo } from './UserInfo'
+import { UsingPlace } from './UserPlace'
 
 export const MyPage = () => {
   return (
     <div
       style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        width: "100%",
-        height: "100%",
-        marginTop: "3%",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        width: '100%',
+        height: '100%',
+        marginTop: '3%',
       }}
     >
       <span
         style={{
-          fontSize: "40px",
-          fontWeight: "bold",
+          fontSize: '40px',
+          fontWeight: 'bold',
         }}
       >
         마이페이지
       </span>
       <div
         style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "100%",
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%',
           flex: 1,
-          flexDirection: "column",
+          flexDirection: 'column',
         }}
       >
         <UserInfo />
@@ -40,5 +40,7 @@ export const MyPage = () => {
         <UsingPlace />
       </div>
     </div>
-  );
-};
+  )
+}
+
+export default MyPage
